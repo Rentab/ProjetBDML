e37ec4eb46b3823c42050e02874121fdf21fdb4a
Rentab <74931058+Rentab@users.noreply.github.com>
2020-11-25 03:26:48 +0100

    Corpus2Vec_V2 optimisation et ameliorations | V3 organisation , commentaires et test "reel"

--- src/com/projetBDML/Main.java
+++ src/com/projetBDML/Main.java
@@ -1,8 +1,7 @@
 package com.projetBDML;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
@@ -17,7 +16,7 @@ public class Main {
         int n = 300;
         ; //dimension
         ArrayList<String> lines = new ArrayList<String>();
-        ArrayList<String> mots = new ArrayList<String>();
+        //List<ArrayList<String>> mots = new ArrayList<ArrayList<String>>();
         File file = new File("D:\\Cours\\GE5_S1\\Big Data & ML\\Code\\CorpusVec\\data\\test.txt");
 
 
@@ -35,31 +34,25 @@ public class Main {
 
         //---------------------------------------Enlever les brackets---------------------------------------------------
 
-
         for (int i = 0; i < lines.size(); i++) {
             lines.set(i, lines.get(i).replaceAll("\\[", ""));
         }
 
-        Writer writer = new FileWriter("D:\\Cours\\GE5_S1\\Big Data & ML\\Code\\CorpusVec\\data\\output.txt");
-
-        for (int i = 0; i < lines.size(); i++) {
-            writer.write(lines.get(i));
+        ArrayList<ArrayList<String>> mots = new ArrayList<ArrayList<String>>();
+        for (int i=0;i<lines.size();i++) {
+            List<String> temp = Arrays.asList(lines.get(i).split("\\s+"));
+            ArrayList<String> temp1 = new ArrayList<String>();
+            temp1.addAll(temp);
+            if (i==0){
+            }
+            else{
+                temp1.remove(0);
+            }
+            mots.add(temp1);
         }
-        writer.close();
 
-        //------------Tout stocker dans des ArrayList pos 0+i mot i pos 1+i-999+i representation Word2Vec---------------
-
-        file = new File("D:\\Cours\\GE5_S1\\Big Data & ML\\Code\\CorpusVec\\data\\output.txt");
-        scr = new Scanner(file).useDelimiter("\\s+");
-
-        while (scr.hasNext()) {
-            mots.add(scr.next());
-        }
-        /* visualisation pour bounds array
-        for (int i=905;i<=1208;i++) {
-            System.out.println(mots.get(i));
-        }
-        */
+        //Structure mots : [0 , mot , x1 , ... , x300 ]
+        //                  0   1      2          301
 
         //---------------------------------------------Test avec phrase ------------------------------------------------
 
@@ -69,16 +62,18 @@ public class Main {
         double[] poidsphrase = new double[n];
         String[] phraseS = phrase.split("\\s+");
 
+
         for (int i = 0; i < phraseS.length; i++) {//on parcourt les mots de la phrase
             for (int j = 0; j < mots.size(); j++) { // on parcourt les mots vectorialises
-                if (phraseS[i].equals(mots.get(j))) { //si on trouve une occurrence
-                    System.out.println("Mot reconnu "+mots.get(j)+ " en position " +i+ " de la phrase");
+                if (phraseS[i].equals(mots.get(j).get(1))) { //si on trouve une occurrence
+                    System.out.println("Mot reconnu "+mots.get(j).get(1)+ " en position " +i+ " de la phrase");
                     for (int l = 0; l < n; l++) { //on somme les vecteurs associes a chaque mot dans le vecteur
-                        // position mot "x" = j , pos coordonnee 1 = j+1  , cordonnee n j+n+i
-                        poidsphrase[l] += Double.valueOf(mots.get(j + l + 1));
+                         //on ajoute dans le poids de la phrase les valeurs pour chaque mot reconnu
+                         poidsphrase[l] += Double.valueOf(mots.get(j).get(l+2));
                     }
                 }
             }
         }
+
     }
 }
